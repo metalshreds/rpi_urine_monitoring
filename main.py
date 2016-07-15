d91aecc689eaee731f14df014037449f47f738b5
Emmanuel Contreras <emmanuel.contreras@wisc.edu>
2016-07-14 21:45:00 -0500

    made some changes

--- main.py
+++ main.py
@@ -20,24 +20,24 @@ def setup_scale():
     dev = MettlerToledoDevice() # Might automatically find device if one available
     # if it is not found automatically, specify port directly
     dev = MettlerToledoDevice(port='/dev/ttyUSB0') # Linux specific port
-    #dev = MettlerToledoDevice(port='/dev/tty.usbmodem262471') # Mac OS X specific port
-    #dev = MettlerToledoDevice(port='COM3') # Windows specific port
+        #dev = MettlerToledoDevice(port='/dev/tty.usbmodem262471') # Mac OS X specific port
+        #dev = MettlerToledoDevice(port='COM3') # Windows specific port
     dev.get_serial_number()
-    #1126493049
+        #1126493049
     dev.get_balance_data()
-    #['XS204', 'Excellence', '220.0090', 'g']
+        #['XS204', 'Excellence', '220.0090', 'g']
     dev.get_weight_stable()
-    #[-0.0082, 'g'] #if weight is stable
-    #None  #if weight is dynamic
+        #[-0.0082, 'g'] #if weight is stable
+        #None  #if weight is dynamic
     dev.get_weight()
-    #[-0.6800, 'g', 'S'] #if weight is stable
-    #[-0.6800, 'g', 'D'] #if weight is dynamic
+        #[-0.6800, 'g', 'S'] #if weight is stable
+        #[-0.6800, 'g', 'D'] #if weight is dynamic
     dev.zero_stable()
-    #True  #zeros if weight is stable
-    #False  #does not zero if weight is not stable
+        #True  #zeros if weight is stable
+        #False  #does not zero if weight is not stable
     dev.zero()
-    #'S'   #zeros if weight is stable
-    #'D'   #zeros if weight is dynamic
+        #'S'   #zeros if weight is stable
+        #'D'   #zeros if weight is dynamic
 
 
 
@@ -58,7 +58,7 @@ def detect_motion(camera):
         prior_image = current_image
         return result
 
-def detect_weight_change():
+#def detect_weight_change():
 
 
 def write_video(stream):
