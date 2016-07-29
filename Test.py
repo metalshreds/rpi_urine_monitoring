23cb0854786bdd36d9ebea9cb56762013463bd19
ajnimunkar <ikshua@gmail.com>
2016-07-29 13:29:00 -0500

    Code for simultaneous acquisition of Arduino data, Video data and time stamp data

--- Test.py
+++ Test.py
@@ -6,7 +6,7 @@ ser = serial.Serial('/dev/ttyACM0',9600)
 weightdata = ser.readline()
 timedata = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-with picamera.Picamera() as camera;
+with picamera.Picamera() as camera:
     camera.resolution = (640, 480)
     camera.start_recording('my_video.h264')
     camera.wait(2)
