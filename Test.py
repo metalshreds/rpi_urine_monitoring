13e9d9ebee104e09a132527c74d2c19e0851ff93
Emmanuel Contreras <emmanuel.contreras@wisc.edu>
2016-07-29 14:07:39 -0500

    Auto-indent

--- Test.py
+++ Test.py
@@ -10,27 +10,27 @@ timedata = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 # with picamera.PiCamera() as camera:
 #   camera.resolution = (640, 480)
 
-    weightdata = ser.readline()
-    timedata = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-    print(weightdata,timedata)
+weightdata = ser.readline()
+timedata = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+print(weightdata,timedata)
 
-    camera = picamera.PiCamera(resolution=(1280, 720), framerate=24)
-    camera.start_preview()
-    camera.annotate_background = picamera.Color('black')
+# camera = picamera.PiCamera(resolution=(1280, 720), framerate=24)
+camera.start_preview()
+camera.annotate_background = picamera.Color('black')
+camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+camera.start_recording('timestamped.h264')
+start = dt.datetime.now()
+while (dt.datetime.now() - start).seconds < 30:
     camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    camera.start_recording('timestamped.h264')
-    start = dt.datetime.now()
-    while (dt.datetime.now() - start).seconds < 30:
-        camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        camera.wait_recording(0.2)
-    camera.stop_recording()
+    camera.wait_recording(0.2)
+camera.stop_recording()
 
-    # camera.start_recording('my_video.h264')
-    # camera.wait_recording(60)
-    # camera.stop_recording()
+# camera.start_recording('my_video.h264')
+# camera.wait_recording(60)
+# camera.stop_recording()
 
-    weightdata = ser.readline()
-    timedata = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-    print(weightdata,timedata)
+weightdata = ser.readline()
+timedata = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+print(weightdata,timedata)
 
 
