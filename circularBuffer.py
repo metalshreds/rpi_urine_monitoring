c96abcaf180bb0736a726bd16ab05ff57b949397
Emmanuel Contreras <emmanuel.contreras@wisc.edu>
2016-08-18 18:33:09 -0500

    updated circular Buffer test

--- circularBuffer.py
+++ circularBuffer.py
@@ -9,8 +9,6 @@ import csv
 
 saveFile = open('timedata.csv', 'aw')
 writer = csv.writer(saveFile)
-camera = picamera.PiCamera()
-stream = picamera.PiCameraCircularIO(camera, seconds=20)
 
 
 def event_detected():
@@ -18,32 +16,28 @@ def event_detected():
     return random.randint(0, 10) == 0
 
 
-def write_file(timestamp):
-    # weightData = event_detected()
-    writer.writerow([timestamp, event_detected()])
-
-
-def video_timestamp():  # camera
-    timestamp = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    camera.annotate_background = picamera.Color('black')
-    camera.annotate_text = timestamp
-    camera.wait_recording(0.2)
-    write_file(timestamp)
-
-
-def write_video():  # stream
+def write_video(stream):
     file_name = strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ".h264"
     stream.copy_to(file_name, seconds=30)
     print('done writing file')
 
 
 # Main #
+camera = picamera.PiCamera()
+stream = picamera.PiCameraCircularIO(camera, seconds=20)
 camera.start_recording(stream, format='h264')
 try:
     while True:
         print('waiting')
+        # # Annotate camera
+        timestamp = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        camera.annotate_background = picamera.Color('black')
+        camera.annotate_text = timestamp
+        # Write File
+        writer.writerow([timestamp, event_detected()])
         camera.wait_recording(0.2)
-        video_timestamp()  # camera
+        # #
+        # video_timestamp(camera)
         if event_detected():
             print('event detected')
             # Keep recording for 10 seconds and only then write the
@@ -51,8 +45,13 @@ try:
             # camera.wait_recording(10)
             start = dt.datetime.now()
             while (dt.datetime.now() - start).seconds < 10:
-                video_timestamp()  # camera
-            write_video()  # stream
+                # # Annotate camera
+                timestamp = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                camera.annotate_background = picamera.Color('black')
+                camera.annotate_text = timestamp
+                # Write File
+                writer.writerow([timestamp, event_detected()])
+            write_video(stream)
 finally:
     camera.stop_recording()
     saveFile.close()
