90a865b32b58bb81d660f895e965ff17aee390cf
ajnimunkar <ikshua@gmail.com>
2016-08-19 12:32:14 -0500

    debug - CSV timestamp

--- circularBuffer.py
+++ circularBuffer.py
@@ -13,7 +13,9 @@ dev = MettlerToledoDevice(port='/dev/ttyACM0') # Linux specific port
 saveFile = open('timedata.csv', 'aw')
 writer = csv.writer(saveFile)
 
-def event_detected():
+
+
+def event_detected(currWeight):
 
     # 5mg change in weight
     # 5ml in volume
@@ -21,11 +23,13 @@ def event_detected():
     # [-0.6800, 'g', 'S'] #if weight is stable
     weight, units, stability = dev.get_weight()
 
-    diffWeight = currWeight-weight
-    if diffWeight < 0:
+    if weight-currWeight >= 0.005:
        currWeight = weight
-    elif
-        diffWeight
+       return True
+    currWeight = weight
+
+
+
 
 
 
@@ -52,11 +56,13 @@ try:
         camera.annotate_background = picamera.Color('black')
         camera.annotate_text = timestamp
         # Write File
-        writer.writerow([timestamp, event_detected()])
+        writer.writerow([timestamp, weight])
         camera.wait_recording(0.2)
         # #
         # video_timestamp(camera)
-        if event_detected():
+        weight, units, stability = dev.get_weight() # get the current weight
+
+        if weight-currWeight >= 0.005:
             print('event detected')
             # Keep recording for 10 seconds and only then write the
             # stream to disk
@@ -68,11 +74,13 @@ try:
                 camera.annotate_background = picamera.Color('black')
                 camera.annotate_text = timestamp
                 # Write File
-                writer.writerow([timestamp, event_detected()])
+                weight, units, stability = dev.get_weight()  # get the current weight
+                writer.writerow([timestamp, weight])
             # Write video to file
             file_name = start.strftime("%Y-%m-%d %H:%M:%S") + ".h264"
             stream.copy_to(file_name, seconds=30)
             print('done writing file')
+        currWeight = weight             # weight updated but no event occurring
 finally:
     camera.stop_recording()
     saveFile.close()
