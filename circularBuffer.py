4250fd7fada06972eb3c23b1d03846e96c14f2b6
Emmanuel Contreras <emmanuel.contreras@wisc.edu>
2016-08-19 11:42:34 -0500

    updated circular Buffer test

--- circularBuffer.py
+++ circularBuffer.py
@@ -4,19 +4,43 @@ import picamera
 import datetime as dt
 from time import gmtime, strftime
 import csv
-# from mettler_toledo_device import MettlerToledoDevice
+from mettler_toledo_device import MettlerToledoDevice
+dev = MettlerToledoDevice() # Might automatically find device if one available
+# if it is not found automatically, specify port directly
+dev = MettlerToledoDevice(port='/dev/ttyACM0') # Linux specific port
 
 
 saveFile = open('timedata.csv', 'aw')
 writer = csv.writer(saveFile)
 
-
 def event_detected():
+
+    # 5mg change in weight
+    # 5ml in volume
+
+    # [-0.6800, 'g', 'S'] #if weight is stable
+    weight, units, stability = dev.get_weight()
+
+    diffWeight = currWeight-weight
+    if diffWeight < 0:
+       currWeight = weight
+    elif
+        diffWeight
+
+
+
+
+
+
+
     # Randomly return True (like a fake motion detection routine)
     return random.randint(0, 10) == 0
 
 
 # Main #
+# get current weight
+currWeight, units, stability = dev.get_weight()
+# start picamera
 camera = picamera.PiCamera()
 stream = picamera.PiCameraCircularIO(camera, seconds=20)
 camera.start_recording(stream, format='h264')
